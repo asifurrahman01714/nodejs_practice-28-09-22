db4c8908af23e5b5b81b5fd95f2faa73ffdd67a7
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-09-28 10:54:05 +0600

    create a user route

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 const express = require('express')
 const app = express()
 
+// create a new route
 app.get('/', (req,res) =>{
     res.send('Hello World, I am a Node.js Developer')
 })
 
+// create a user route
+const users = ["asif", "sakib", "arif", "azad"];
+app.get('/user/:id', (req,res)=>{
+    const id = req.params.id;
+    console.log(id)
+    const name = users[id];
+    res.send({id,name});
+})
 // Running the server on port 3000
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
