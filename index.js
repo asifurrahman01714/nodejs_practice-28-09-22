ed78312ba12746dbea0e71c82211f7b9b1cdee07
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-10-21 21:43:00 +0600

    deleted product successfully

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const bodyParser = require('body-parser');
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
 
 // Middle ware apps
 app.use(bodyParser.json());
@@ -29,9 +31,20 @@ client.connect(err => {
     })
   })
 
+  // Deleting Items
+  app.delete('/delete/:id',(req, res)=>{
+    const productId = req.params.id;
+    console.log(productId);
+
+    productCollection.deleteOne({_id: ObjectId(productId)})
+    .then(result=>{
+        console.log(result.deletedCount);
+    })
+  })
+
   // Get data from mongodb
   app.get('/products',(req,res)=>{
-    productCollection.find({}).limit(4).toArray(function(err, result) {
+    productCollection.find({}).toArray(function(err, result) {
         if (err) throw err;
         console.log(result);
         res.send(result)
